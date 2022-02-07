ff9faca0552bfbc96fc0e4cd3cdd4fac8b6b2f73
emmanuelchinaza5777@gmail.com <emmanuelchinaza5777@gmail.com>
2022-02-07 20:50:20 +0100

    solutions

--- assessments/dist/sumOfSquares.js
+++ assessments/dist/sumOfSquares.js
@@ -8,5 +8,10 @@ const sumOfSquares = (numberSet) => {
         const squareOfNumber = square(num);
         summedSquares += squareOfNumber;
     });
+    console.log("\n\t Sum of squares: ", summedSquares);
     return summedSquares;
 };
+const main = () => {
+    sumOfSquares([3, 5, 18, 24, 12]);
+};
+main();
